3a19bc23af9915c1aca242444c8997491d07b273
rhun27 <83758444+rhun27@users.noreply.github.com>
2021-07-19 12:25:40 +0200

    Delete src directory

--- src/main/java/esAgenzia/dal/UtenteDAO.java
+++
@@ -1,9 +0,0 @@
-package esAgenzia.dal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import esAgenzia.entities.Utente;
-
-public interface UtenteDAO extends JpaRepository<Utente, Integer> {
-
-}
